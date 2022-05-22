3257346ec242e4d7613fa9952d25811494031821
Sultan <hasenovsultanbek@gmail.com>
2022-05-22 18:46:44 +0600

    Added chat service

---
+++ chat_service/Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using StackExchange.Redis;
+
+namespace ChatService.Hubs;
+
+public class ChatHub : Hub
+{
+    private readonly IDatabase db;
+    public ChatHub(IConnectionMultiplexer redis) => db = redis.GetDatabase();
+
+    public async Task Send(string message, string username)
+    {
+        var lotId = await db.StringGetAsync(Context.ConnectionId);
+        await Clients.Group(lotId).SendAsync("Send", message, username);
+    }
+
+    public async Task AddToChat(string lotId)
+    {
+        await db.StringSetAsync(Context.ConnectionId, lotId).ContinueWith(_ => Groups.AddToGroupAsync(Context.ConnectionId, lotId));
+    }
+}
---
+++ chat_service/Program.cs
@@ -0,0 +1,26 @@
+using ChatService.Hubs;
+using StackExchange.Redis;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+#region Chat provider
+builder.Services.AddSignalR();
+#endregion
+#region Redis
+ConnectionMultiplexer.Connect(builder.Configuration["ConnectionStrings:Redis"]).GetDatabase().StringSet("a", "b");
+builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(builder.Configuration["ConnectionStrings:Redis"]));
+#endregion
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseAuthorization();
+app.MapControllers();
+app.MapHub<ChatHub>("/chat");
+app.Run();
